59fd2ec2546e3b1d584c260bb6e7a25e4e860356
Tushar Banik <b.toshar@iitg.ac.in>
2025-09-03 22:49:40 +0530

    feat: setup mapbox

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,31 +1,33 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import helmet from 'helmet'
-import ratelimit from 'express-rate-limit'
-import morgan from 'morgan'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import helmet from "helmet";
+import ratelimit from "express-rate-limit";
+import morgan from "morgan";
+import userRoutes from "./modules/user/user.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(ratelimit({ windowMs: 15 * 60 * 1000, max: 100 }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.get('/hello', (req, res) => {
-    res.send('Hello from API v1');
-});
-
 app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+    res.send("Hello from API v1");
+});
+app.use('/api/v1/users', userRoutes);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+  console.log(`server is running on port: ${port}`);
+});
